ba51728e64384bc95b217f282d3b1372b07cfd58
MarcusJones <jones.0bj3@gmail.com>
2019-07-13 10:03:06 +0200

    update

--- udacity-c2-basic-server/src/server.ts
+++ udacity-c2-basic-server/src/server.ts
@@ -18,15 +18,20 @@ import { Car, cars as cars_list } from './cars';
   //are accessable as req.body.{{variable}}
   app.use(bodyParser.json()); 
 
-
+  // HANDLERS
+  // Root URI call
   let getRoot = ( req: Request, res: Response ) => {
     console.log("Get root");
     
     res.status(200).send("Welcome to the Cloud!");
   } 
-
-
+  
+  // Get a greeting to a specific person 
+  // to demonstrate routing parameters
+  // > try it {{host}}/persons/:the_name
   let getPersonsName = ( req: Request, res: Response ) => {
+    console.log("getPersonsName");
+    
     let { name } = req.params;
 
     if ( !name ) {
@@ -37,18 +42,11 @@ import { Car, cars as cars_list } from './cars';
     return res.status(200)
               .send(`Welcome to the Cloud, ${name}!`);
   }
-  // Root URI call
-  app.get( "/", getRoot);
-
-  // Get a greeting to a specific person 
-  // to demonstrate routing parameters
-  // > try it {{host}}/persons/:the_name
-  app.get( "/persons/:name", getPersonsName
-     );
 
   // Get a greeting to a specific person to demonstrate req.query
   // > try it {{host}}/persons?name=the_name
-  app.get( "/persons/", ( req: Request, res: Response ) => {
+  let getPersonsNameQuery = ( req: Request, res: Response ) => {
+    console.log("getPersonsNameQuery");
     let { name } = req.query;
 
     if ( !name ) {
@@ -58,25 +56,33 @@ import { Car, cars as cars_list } from './cars';
 
     return res.status(200)
               .send(`Welcome to the Cloud, ${name}!`);
-  } );
+  }
+
 
   // Post a greeting to a specific person
   // to demonstrate req.body
   // > try it by posting {"name": "the_name" } as 
   // an application/json body to {{host}}/persons
-  app.post( "/persons", 
-    async ( req: Request, res: Response ) => {
+  let getPersonsNameBody = async ( req: Request, res: Response ) => {
+    console.log("getPersonsNameBody");
+    console.log(req.body);
+    
+    const { name } = req.body;
 
-      const { name } = req.body;
+    if ( !name ) {
+      return res.status(400)
+                .send(`name is required`);
+    }
 
-      if ( !name ) {
-        return res.status(400)
-                  .send(`name is required`);
-      }
+    return res.status(200)
+              .send(`Welcome to the Cloud, ${name}!`);
+  }
 
-      return res.status(200)
-                .send(`Welcome to the Cloud, ${name}!`);
-  } );
+  // ROUTES
+  app.get( "/", getRoot);
+  app.get( "/persons/:name", getPersonsName);
+  app.get( "/persons/",  getPersonsNameQuery);
+  app.post( "/persons", getPersonsNameBody);
 
   // @TODO Add an endpoint to GET a list of cars
   // it should be filterable by make with a query paramater
@@ -90,7 +96,7 @@ import { Car, cars as cars_list } from './cars';
 
   // Start the Server
   app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
+      console.log( `\n \n server running http://localhost:${ port }` );
       console.log( `press CTRL+C to stop server` );
   } );
 })();

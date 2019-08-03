880384ce1e1619c9f8649f86cd169a329da068fe
MarcusJones <jones.0bj3@gmail.com>
2019-08-03 13:46:55 +0200

    update routes and config

--- udacity-c2-restapi-dev/src/config/config.ts
+++ udacity-c2-restapi-dev/src/config/config.ts
@@ -1,14 +1,14 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
-    "dialect": "postgres",
-    "aws_reigion": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
-  },
+    "username":         process.env.POSTGRES_USERNAME,
+    "password":         process.env.POSTGRES_PASSWORD,
+    "database":         process.env.POSTGRES_DATABASE,
+    "host":             process.env.POSTGRES_HOST,
+    "dialect":          "postgres",
+    "aws_reigion":      process.env.POSTGRES_AWS_REGION,
+    "aws_profile":      process.env.POSTGRES_AWS_PROFILE,
+    "aws_media_bucket": process.env.POSTGRES_AWS_MEDIA_BUCKET
+  },  
   "prod": {
     "username": "",
     "password": "",
